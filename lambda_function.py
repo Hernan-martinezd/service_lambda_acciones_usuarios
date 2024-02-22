e6f1c0a59c34f2ffb69b8fc64ea933569ebab677
Hernan <hd.martinezd1@uniandes.edu.co>
2024-02-21 22:49:54 -0500

    Lambda_function modificado

--- lambda_function.py
+++ lambda_function.py
@@ -3,24 +3,9 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def intex():
-    return jsonify(status=200, message='OK')
-
 @app.route('/monitor')
 def monitor():
     return jsonify(status= 200, message= 'Echo: I\'m good')
-
-@app.route('/health')
-def health_data():
-    health_metrics = {
-        "frecuencia cardíaca": 72,  # bpm 
-        "ftp": 250,                 # Functional Threshold Power en watts 
-        "saturación": 98,           # Porcentaje de saturación de oxígeno
-        "velocidad": 5.5,           # km/h
-        "distancia recorrida": 10.2 # Km
-    }
-    return jsonify(status= 200, message= health_metrics)
 
 def lambda_handler(event, context):
     return awsgi.response(app, event, context, base64_content_types={"image/png"})
