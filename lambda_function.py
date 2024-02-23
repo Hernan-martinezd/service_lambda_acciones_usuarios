30374cda958d90b5a65aa5a529234989479f945d
Hernan <hd.martinezd1@uniandes.edu.co>
2024-02-22 21:38:17 -0500

    Prueba de registro de usuario

--- lambda_function.py
+++ lambda_function.py
@@ -5,8 +5,19 @@ app = Flask(__name__)
 
 @app.route('/monitor')
 def monitor():
-    return jsonify(status= 200, message= 'Echo: I\'m good')
+    return jsonify(status=200, message='Echo: I\'m good')
 
+@app.route('/register')
+def register():
+    register_data = {
+        "Nombre": "Juan",
+        "Apellido": "Perez",
+        "Edad": 25,
+        "Email": "123@123.com",
+        "Telefono": "1234567890"
+        }
+    
+    return jsonify(status=200, message='Usuario creado exitosamente')
+    
 def lambda_handler(event, context):
     return awsgi.response(app, event, context, base64_content_types={"image/png"})
-
